ac0fd8d720ee1fdb62ffefbba6bfc94e94ec2b53
Niccolò Capitelli <niccolo.capitelli@kalpa.it>
2022-07-13 17:22:30 +0200

    Checkpoint #2: parser

--- app/src/main/java/com/nik/marketapp/data/repository/StockRepositoryImpl.kt
+++ app/src/main/java/com/nik/marketapp/data/repository/StockRepositoryImpl.kt
@@ -1,7 +1,10 @@
 package com.nik.marketapp.data.repository
 
+import com.nik.marketapp.data.csv.CSVParser
+import com.nik.marketapp.data.csv.CompanyListingsParser
 import com.nik.marketapp.data.local.StockDatabase
 import com.nik.marketapp.data.mapper.toCompanyListing
+import com.nik.marketapp.data.mapper.toCompanyListingEntity
 import com.nik.marketapp.data.remote.StockApi
 import com.nik.marketapp.domain.model.CompanyListing
 import com.nik.marketapp.domain.repository.StockRepository
@@ -16,7 +19,8 @@ import javax.inject.Singleton
 @Singleton
 class StockRepositoryImpl @Inject constructor(
     val api: StockApi,
-    val db: StockDatabase
+    val db: StockDatabase,
+    val companyListingsParser: CSVParser<CompanyListing>
 ): StockRepository {
 
     private val dao = db.dao
@@ -40,13 +44,27 @@ class StockRepositoryImpl @Inject constructor(
             }
             val remoteListings = try {
                 val response = api.getListings()
-                response.byteStream()
+                companyListingsParser.parse(response.byteStream())
             } catch (e: IOException) {
                 e.printStackTrace()
                 emit(Resource.Error("Couldn't load data"))
+                null
             } catch (e: HttpException) {
                 e.printStackTrace()
                 emit(Resource.Error("Couldn't load data"))
+                null
+            }
+
+            remoteListings?.let { listings ->
+                dao.clearCompanyListings()
+                dao.insertCompanyListings(
+                    listings.map { it.toCompanyListingEntity() }
+                )
+                emit(Resource.Success(
+                    data = dao.searchCompanyListing("")
+                        .map { it.toCompanyListing() }
+                ))
+                emit(Resource.Loading(false))
             }
         }
     }
