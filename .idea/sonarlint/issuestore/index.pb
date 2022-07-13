ea881a13c23d836ea14e134b93d9cfb91e5c25c1
Niccolò Capitelli <niccolo.capitelli@kalpa.it>
2022-07-13 14:42:49 +0200

    init

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,17 @@
+
+d
+4app/src/main/java/com/nik/marketapp/ui/theme/Type.kt,8/c/8c851992acdafec6a2d6a1111bf4c5d2a7210dee
+e
+5app/src/main/java/com/nik/marketapp/ui/theme/Theme.kt,d/9/d9e99f201d7560d2e1df4ce4fcf50df2923b0635
+e
+5app/src/main/java/com/nik/marketapp/ui/theme/Shape.kt,0/1/01808cca67cf4241240c61c5b5a6377f70802f47
+e
+5app/src/main/java/com/nik/marketapp/ui/theme/Color.kt,e/3/e3a725d9a7189fd5285e3108ab54af1dbcca3ad3
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
+c
+3app/src/main/java/com/nik/marketapp/MainActivity.kt,8/d/8d6010f7684327f7c2a3824f358c5b6aaab9b393
+P
+ app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
---
+++ app/src/main/java/com/nik/marketapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.nik.marketapp
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.nik.marketapp.ui.theme.MarketAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MarketAppTheme {
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ){
+
+                }
+            }
+        }
+    }
+}
